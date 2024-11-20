36a87bd5b9b6d410fd923f158999f99253a2dbfc
chouchouji <1305974212@qq.com>
2024-11-20 20:27:09 +0800

    init project

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig((option) => ({
+  entry: ['src/extension.ts'],
+  external: ['vscode'],
+  splitting: false,
+  clean: true,
+  format: ['cjs'],
+  outDir: 'dist',
+  sourcemap: true,
+  minify: !option.watch,
+}));
