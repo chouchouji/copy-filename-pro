e9a37360792b214f969a091b52c52ed33ec975c0
chouchouji <1305974212@qq.com>
2024-11-20 20:28:18 +0800

    feat: support copying filename and directory

---
+++ src/extension.ts
@@ -0,0 +1,49 @@
+import * as vscode from 'vscode';
+import fs from 'node:fs';
+import path from 'node:path';
+
+export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand('copy-filename-pro.copyFileNameWithExtension', (uri: vscode.Uri) => {
+      const fsPath = uri.fsPath;
+      fs.stat(fsPath, (err: NodeJS.ErrnoException | null, stats: fs.Stats) => {
+        if (err) {
+          vscode.window.showErrorMessage(err.message);
+          return;
+        }
+
+        vscode.env.clipboard.writeText(path.basename(fsPath));
+      });
+    }),
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand('copy-filename-pro.copyFileNameNoExtension', (uri: vscode.Uri) => {
+      const fsPath = uri.fsPath;
+      fs.stat(fsPath, (err: NodeJS.ErrnoException | null, stats: fs.Stats) => {
+        if (err) {
+          vscode.window.showErrorMessage(err.message);
+          return;
+        }
+
+        vscode.env.clipboard.writeText(path.basename(fsPath, path.extname(fsPath)));
+      });
+    }),
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand('copy-filename-pro.copyDirectory', (uri: vscode.Uri) => {
+      const fsPath = uri.fsPath;
+      fs.stat(fsPath, (err: NodeJS.ErrnoException | null, stats: fs.Stats) => {
+        if (err) {
+          vscode.window.showErrorMessage(err.message);
+          return;
+        }
+
+        vscode.env.clipboard.writeText(path.basename(fsPath));
+      });
+    }),
+  );
+}
+
+export function deactivate() {}
