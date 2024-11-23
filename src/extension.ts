7c2ff6a8a043001aa7e22de2129f1af1ea3f3ad8
chouchouji <1305974212@qq.com>
2024-11-23 18:21:15 +0800

    feat: support getting relative path without extension

--- src/extension.ts
+++ src/extension.ts
@@ -23,6 +23,17 @@ export function activate(context: vscode.ExtensionContext) {
   );
 
   context.subscriptions.push(
+    vscode.commands.registerCommand('copy-filename-pro.copyRelativePathWithoutExtension', (uri: vscode.Uri) => {
+      const fsPath = uri.fsPath;
+      const workspaceFolder = vscode.workspace.getWorkspaceFolder(vscode.Uri.file(fsPath));
+      if (workspaceFolder) {
+        const { dir, name } = path.parse(path.relative(workspaceFolder.uri.fsPath, fsPath));
+        vscode.env.clipboard.writeText(path.join(dir, '/', name));
+      }
+    }),
+  );
+
+  context.subscriptions.push(
     vscode.commands.registerCommand('copy-filename-pro.copyDirectory', (uri: vscode.Uri) => {
       vscode.env.clipboard.writeText(path.basename(uri.fsPath));
     }),
