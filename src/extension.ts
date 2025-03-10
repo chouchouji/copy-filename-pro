f81511ab98936f30fb54257945206b6483ff251d
chouchouji <1305974212@qq.com>
2025-03-10 14:07:30 +0800

    refactor: streamline file handling in copy commands

--- src/extension.ts
+++ src/extension.ts
@@ -7,13 +7,14 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.commands.registerCommand(
       'copy-filename-pro.copyFileNameWithExtension',
-      (_uri: vscode.Uri, files?: Array<vscode.Uri>) => {
-        if (isNonEmptyArray(files)) {
-          const fileNames = files
-            .filter((file) => fs.statSync(file.fsPath).isFile())
-            .map((file) => path.basename(file.fsPath))
-          vscode.env.clipboard.writeText(fileNames.join('\n'))
+      (uri: vscode.Uri, files?: Array<vscode.Uri>) => {
+        const uris = isNonEmptyArray(files) ? files : uri ? [uri] : []
+        if (uris.length === 0) {
+          return
         }
+
+        const fileNames = uris.filter((uri) => fs.statSync(uri.fsPath).isFile()).map((uri) => path.basename(uri.fsPath))
+        vscode.env.clipboard.writeText(fileNames.join('\n'))
       },
     ),
   )
@@ -21,13 +22,16 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.commands.registerCommand(
       'copy-filename-pro.copyFileNameWithoutExtension',
-      (_uri: vscode.Uri, files?: Array<vscode.Uri>) => {
-        if (isNonEmptyArray(files)) {
-          const fileNames = files
-            .filter((file) => fs.statSync(file.fsPath).isFile())
-            .map((file) => path.basename(file.fsPath, path.extname(file.fsPath)))
-          vscode.env.clipboard.writeText(fileNames.join('\n'))
+      (uri: vscode.Uri, files?: Array<vscode.Uri>) => {
+        const uris = isNonEmptyArray(files) ? files : uri ? [uri] : []
+        if (uris.length === 0) {
+          return
         }
+
+        const fileNames = uris
+          .filter((uri) => fs.statSync(uri.fsPath).isFile())
+          .map((uri) => path.basename(uri.fsPath, path.extname(uri.fsPath)))
+        vscode.env.clipboard.writeText(fileNames.join('\n'))
       },
     ),
   )
@@ -35,14 +39,17 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.commands.registerCommand(
       'copy-filename-pro.copyPathWithoutExtension',
-      (_uri: vscode.Uri, files?: Array<vscode.Uri>) => {
-        if (isNonEmptyArray(files)) {
-          const fileNames = files.map((file) => {
-            const { dir, name } = path.parse(file.fsPath)
-            return path.join(dir, '/', name)
-          })
-          vscode.env.clipboard.writeText(fileNames.join('\n'))
+      (uri: vscode.Uri, files?: Array<vscode.Uri>) => {
+        const uris = isNonEmptyArray(files) ? files : uri ? [uri] : []
+        if (uris.length === 0) {
+          return
         }
+
+        const fileNames = uris.map((uri) => {
+          const { dir, name } = path.parse(uri.fsPath)
+          return path.join(dir, '/', name)
+        })
+        vscode.env.clipboard.writeText(fileNames.join('\n'))
       },
     ),
   )
@@ -50,19 +57,22 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.commands.registerCommand(
       'copy-filename-pro.copyRelativePathWithoutExtension',
-      (_uri: vscode.Uri, files?: Array<vscode.Uri>) => {
-        if (isNonEmptyArray(files)) {
-          const fileNames = files.reduce((files: string[], file) => {
-            const workspaceFolder = vscode.workspace.getWorkspaceFolder(vscode.Uri.file(file.fsPath))
-            if (workspaceFolder) {
-              const { dir, name } = path.parse(path.relative(workspaceFolder.uri.fsPath, file.fsPath))
-              files.push(path.join(dir, '/', name))
-            }
-
-            return files
-          }, [])
-          vscode.env.clipboard.writeText(fileNames.join('\n'))
+      (uri: vscode.Uri, files?: Array<vscode.Uri>) => {
+        const uris = isNonEmptyArray(files) ? files : uri ? [uri] : []
+        if (uris.length === 0) {
+          return
         }
+
+        const fileNames = uris.reduce((files: string[], uri) => {
+          const workspaceFolder = vscode.workspace.getWorkspaceFolder(vscode.Uri.file(uri.fsPath))
+          if (workspaceFolder) {
+            const { dir, name } = path.parse(path.relative(workspaceFolder.uri.fsPath, uri.fsPath))
+            files.push(path.join(dir, '/', name))
+          }
+
+          return files
+        }, [])
+        vscode.env.clipboard.writeText(fileNames.join('\n'))
       },
     ),
   )
